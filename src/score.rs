0e8726368021deec5128d7cdc2e6b606a04753a0
Brad Gibson <brad@humanenginuity.com>
2019-10-11 06:35:08 -0700

    readability tweaks

--- src/score.rs
+++ src/score.rs
@@ -3,47 +3,56 @@ mod unit_tests;
 
 use crate::Rolls;
 
+const MAX_FRAMES_PER_GAME: usize = 10;
 const MAX_ROLLS_PER_FRAME: usize = 2;
 const MAX_PIN_SCORE_PER_FRAME: u8 = 10;
 const ROLLS_PER_STRIKE: usize = 1;
 const ROLLS_PER_SPARE: usize = 2;
 
-#[allow(clippy::needless_pass_by_value)]
-pub fn score(rolls: Rolls) -> u16 { score_by_frame(rolls.0.iter(), 1) }
+pub fn score(rolls: &Rolls) -> u16 {
+    score_by_frame(rolls.0.iter(), 1)
+}
 
 fn score_by_frame<'a, I>(mut iter: I, frame: usize) -> u16
 where
     I: ExactSizeIterator<Item = &'a u8> + Clone,
 {
-    if frame > 10 || iter.len() == 0 {
-        return 0
+    if frame > MAX_FRAMES_PER_GAME || iter.len() == 0 {
+        return 0;
     }
 
     let mut frame_score = 0;
 
     // max `frame_score` is 10 which cannot overflow a `u8`
-    // max `i + 1` == 2 which cannot overflow a `u8`
-    // max `count + 1` == 2 which cannot overflow a `u8`
+    // max `roll + 1` == 2 which cannot overflow a `u8`
+    // max `.count() + 1` == 2 which cannot overflow a `u8`
     //     Note: `+ 1` req'd because `take_while()` 'consumes' final element (removed from src, but not in dst iterator
     #[allow(clippy::integer_arithmetic)]
-    let rolls_this_frame = iter.by_ref()
-                               .enumerate()
-                               .take_while(|&(i, &v)| {
-                                   frame_score += v;
-                                   frame_score < MAX_PIN_SCORE_PER_FRAME && i + 1 < MAX_ROLLS_PER_FRAME
-                               })
-                               .count() + 1;
+    let rolls_this_frame = iter
+        .by_ref()
+        .enumerate()
+        .take_while(|&(roll, &roll_score)| {
+            frame_score += roll_score;
+            frame_score < MAX_PIN_SCORE_PER_FRAME && roll + 1 < MAX_ROLLS_PER_FRAME
+        })
+        .count()
+        + 1;
 
     let bonus_iter = iter.clone();
-    let bonus_score = bonus_iter.take(match (frame_score, rolls_this_frame) {
-                                    (MAX_PIN_SCORE_PER_FRAME, ROLLS_PER_STRIKE) => 2,
-                                    (MAX_PIN_SCORE_PER_FRAME, ROLLS_PER_SPARE) => 1,
-                                    _ => 0,
-                                })
-                                .sum::<u8>();
+    let bonus_score = bonus_iter
+        .take(match (frame_score, rolls_this_frame) {
+            (MAX_PIN_SCORE_PER_FRAME, ROLLS_PER_STRIKE) => 2,
+            (MAX_PIN_SCORE_PER_FRAME, ROLLS_PER_SPARE) => 1,
+            _ => 0,
+        })
+        .sum::<u8>();
 
     // max `frame_score + bonus score` == 30 which cannot overflow a `u8`
     // max cumulative score for rest of game == 270 which cannot overflow a `u16`
+    // ∴ max cumulative score == 300 which cannot overflow a `u16`
     #[allow(clippy::integer_arithmetic)]
-    { u16::from(frame_score + bonus_score) + score_by_frame(iter, frame + 1) }
+    // braces due to rustc: 1.38.0: error[E0658]: attributes on expressions are experimental
+    {
+        u16::from(frame_score + bonus_score) + score_by_frame(iter, frame + 1)
+    }
 }
--- src/score/unit_tests.rs
+++ src/score/unit_tests.rs
@@ -1,91 +1,92 @@
+#![allow(non_snake_case)]
 use super::*;
 use crate::Result;
 
 #[test]
-fn calc_no_rolls_returns_0() -> Result<()> {
+fn score__no_rolls_returns_0() -> Result<()> {
     // given an empty `Rolls`
     let rolls = Rolls::new(&[])?;
 
     // when invoked
-    let res = score(rolls);
+    let res = score(&rolls);
 
     // the score should be 0
-    assert_eq!(0, res);
+    assert_eq!(res, 0);
     Ok(())
 }
 
 #[test]
-fn calc_0_returns_0() -> Result<()> {
+fn score__0_returns_0() -> Result<()> {
     // given an empty `Rolls`
     let rolls = Rolls::new(&[0])?;
 
     // when invoked
-    let res = score(rolls);
+    let res = score(&rolls);
 
     // the score should be 0
-    assert_eq!(0, res);
+    assert_eq!(res, 0);
     Ok(())
 }
 
 #[test]
-fn calc_all_0s_returns_0() -> Result<()> {
+fn score__all_0s_returns_0() -> Result<()> {
     // given an empty `Rolls`
     let rolls = Rolls::new(&[0; 20])?;
 
     // when invoked
-    let res = score(rolls);
+    let res = score(&rolls);
 
     // the score should be 0
-    assert_eq!(0, res);
+    assert_eq!(res, 0);
     Ok(())
 }
 
 #[test]
-fn calc_1_returns_1() -> Result<()> {
+fn score__1_returns_1() -> Result<()> {
     // given a 1-roll
     let rolls = Rolls::new(&[1])?;
 
     // when invoked
-    let res = score(rolls);
+    let res = score(&rolls);
 
     // the score should be 1
-    assert_eq!(1, res);
+    assert_eq!(res, 1);
     Ok(())
 }
 
 #[test]
-fn calc_all_1s_returns_20() -> Result<()> {
+fn score__all_1s_returns_20() -> Result<()> {
     // given an empty `Rolls`
     let rolls = Rolls::new(&[1; 20])?;
 
     // when invoked
-    let res = score(rolls);
+    let res = score(&rolls);
 
     // the score should be 20
-    assert_eq!(20, res);
+    assert_eq!(res, 20);
     Ok(())
 }
 
 #[test]
-fn calc_5_5_5_returns_20() -> Result<()> {
+fn score__5_5_5_returns_20() -> Result<()> {
     // given an empty `Rolls`
     let rolls = Rolls::new(&[5; 3])?;
 
     // when invoked
-    let res = score(rolls);
+    let res = score(&rolls);
 
     // the score should be 20
-    assert_eq!(20, res);
+    assert_eq!(res, 20);
     Ok(())
 }
 
 #[test]
-fn calc_all_5s_returns_150() -> Result<()> {
+fn score__all_5s_returns_150() -> Result<()> {
     // given an empty `Rolls`
     let rolls = Rolls::new(&[5; 21])?;
 
     // when invoked
-    let res = score(rolls);
+    let res = score(&rolls);
 
     // the score should be 150
     assert_eq!(150, res);
@@ -93,28 +94,27 @@ fn calc_all_5s_returns_150() -> Result<()> {
 }
 
 #[test]
-fn calc_10_10_10_returns_20() -> Result<()> {
+fn score__10_10_10_returns_20() -> Result<()> {
     // given an empty `Rolls`
     let rolls = Rolls::new(&[10; 3])?;
 
     // when invoked
-    let res = score(rolls);
+    let res = score(&rolls);
 
     // the score should be 60
-    assert_eq!(60, res);
+    assert_eq!(res, 60);
     Ok(())
 }
 
 #[test]
-fn calc_all_10s_returns_300() -> Result<()> {
+fn score__all_10s_returns_300() -> Result<()> {
     // given an empty `Rolls`
     let rolls = Rolls::new(&[10; 22])?;
 
     // when invoked
-    let res = score(rolls);
+    let res = score(&rolls);
 
     // the score should be 300
-    assert_eq!(300, res);
+    assert_eq!(res, 300);
     Ok(())
 }
-
